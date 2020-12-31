a85b1bb291716d051c61c3d650502b0dfcb2ec35
Lamija <lamija_dzambo@hotmail.com>
2020-12-31 14:38:40 +0100

    Order and OrderExecutor classes added

--- app/Http/Controllers/Atemschutzmasken/ExcelController.php
+++ app/Http/Controllers/Atemschutzmasken/ExcelController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Atemschutzmasken;
 use App\AtemschutzmaskenClasses\AttributesService;
 
 //use App\Imports\BulkImport;
+use App\AtemschutzmaskenClasses\OrderExecutor;
+use App\AtemschutzmaskenClasses\OrderService;
 use App\AtemschutzmaskenClasses\ProductAttributesService;
 use App\AtemschutzmaskenClasses\ExcelService;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Repositories\ApplicationRepositoryInterface;
+use Automattic\WooCommerce\Client;
+
 
 class ExcelController extends Controller
 {
@@ -23,7 +27,10 @@ class ExcelController extends Controller
     public function index($project_id)
     {
         $allOrders = Order::where('project_id', $project_id)->get();
-        return view('filtered.index', compact('allOrders', 'project_id'));
+
+        $savedOrder = OrderExecutor::save($allOrders, $project_id);
+
+        return view('filtered.index', compact('project_id', 'savedOrder'));
     }
 
     public function exportOrdersExcel(){
