65b562d2ff36a3c48ce02655fcaef22f0b4e7857
AlmirMBH <almir.mustafic.tz@gmail.com>
2021-01-11 10:48:15 +0100

    OrderController edditing

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Services\ApiKeys;
 use App\Services\OrderModel;
-use App\Http\Controllers\Controller;
 use App\Models\Order;
 
 use Illuminate\Http\Request;
