747a7a98a816c2b76446e3ca78cfabde3bdb57c4
Lamija Dzambo <lamija_dzambo@hotmail.com>
2021-01-22 16:04:34 +0100

    Exporting Word File to Google Drive

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -79,10 +79,10 @@
                                                 <a href="{{ route('generate-doc', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$man]) }}" class="my-1" title="WORD MAN"><i class="fas fa-file-word button-icon"></i></a>
                                                 <a href="{{ route('generate-doc', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$woman]) }}" class="my-1" title="WORD WOMAN"><i class="fas fa-file-word button-icon"></i></a>
                                             </div>
-                                            <div class="mr-2 d-flex flex-column">
-                                                <a href="{{route('export-invoices', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$man])}}" class="my-1" title="GOOGLE DRIVE MAN"><i class="fab fa-google-drive button-icon"></i></a>
-                                                <a href="{{route('export-invoices', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$woman])}}" class="my-1" title="GOOLE DRIVE WOMAN"><i class="fab fa-google-drive button-icon"></i></a>
-                                            </div>
+{{--                                            <div class="mr-2 d-flex flex-column">--}}
+{{--                                                <a href="{{route('export-invoices', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$man])}}" class="my-1" title="GOOGLE DRIVE MAN"><i class="fab fa-google-drive button-icon"></i></a>--}}
+{{--                                                <a href="{{route('export-invoices', ['project_id'=>$project_id, 'id'=>$order->id, 'customer_id' => App\Models\Order::$woman])}}" class="my-1" title="GOOLE DRIVE WOMAN"><i class="fab fa-google-drive button-icon"></i></a>--}}
+{{--                                            </div>--}}
                                         </div>
                                     </td>
                                     <td>
