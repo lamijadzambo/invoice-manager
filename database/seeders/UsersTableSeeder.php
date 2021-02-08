f3ebf3b21a23f09309c321cdb34128c8d81c52e0
AlmirMBH <almir.mustafic.tz@gmail.com>
2021-02-08 14:16:00 +0100

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,16 +16,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user1 = new User();
-        $user1->name = 'Die Werber';
-        $user1->email = 'info@die-werber.ch';
-        $user1->password =  Hash::make('4ceFsTe7CJNmGH82YbT4');
+        $user1->name = 'Almir';
+        $user1->email = 'almir@almir.ba';
+        $user1->password =  Hash::make('almir2021');
         $user1->save();
-
-        $user2 = new User();
-        $user2->name = 'swissweb-admin';
-        $user2->email = 'contact@die-werber.ch';
-        $user2->password = Hash::make('4ceFsTe7CJNmGH82YbT4');
-        $user2->save();
     }
 
 }
